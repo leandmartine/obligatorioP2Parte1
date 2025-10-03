7e7fa9a3e7869fe0a0ea42040bae41f7d5efc96f
Renzo823 <rodriguezr200513@gmail.com>
2025-10-03 10:45:45 -0300

    creacion de proyecto

---
+++ Obligatorio Martinez-Rodriguez Parte1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Obligatorio_Martinez_Rodriguez_Parte1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
