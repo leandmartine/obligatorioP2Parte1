529acca8b74940d972273c5df8d8ec4863bd98ca
Renzo823 <rodriguezr200513@gmail.com>
2025-10-03 11:28:53 -0300

    creacion de clases

--- Dominio/obj/Debug/net8.0/Dominio.AssemblyInfo.cs
+++ Dominio/obj/Debug/net8.0/Dominio.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Obligatorio Martinez-Rodriguez Parte1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Dominio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43b999e54d48c07ebef742b8e6fc87a161b2d092")]
-[assembly: System.Reflection.AssemblyProductAttribute("Obligatorio Martinez-Rodriguez Parte1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Obligatorio Martinez-Rodriguez Parte1")]
+[assembly: System.Reflection.AssemblyProductAttribute("Dominio")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Dominio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
